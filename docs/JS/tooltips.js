40836042d64f476c25f088c4130908af6c7e0c90
felipiyocarvajal <felipiyocarvajal@gmail.com>
2024-11-17 13:44:13 -0500

    Finalización info proyectos

--- docs/JS/tooltips.js
+++ docs/JS/tooltips.js
@@ -1,10 +1,16 @@
 tippy('.iconTooltip', {
     content(reference) {
         // Extrae el valor de "data-nameIcon" para cada elemento
-        return reference.getAttribute('data-nameIcon');
+        const content = reference.getAttribute('data-nameIcon');
+        // Si no hay contenido, retorna null para evitar mostrar el tooltip
+        return content ? content : null;
     },
     theme: 'tooltip',
     animation: 'scale-extreme',
     placement: 'bottom',
     fallbackPlacements: ['top'],
+    onShow(instance) {
+        // Si el contenido es null, cancela el tooltip
+        return instance.props.content !== null;
+    },
 });
