f60c6a6f9f26e655d44a3f279a6c032e383c536e
FelipeCarvajalParra <felipiyocarvajal@gmail.com>
2024-11-18 09:51:15 -0500

    Revision 4

--- docs/JS/nav.js
+++ docs/JS/nav.js
@@ -1,3 +1,4 @@
+let lastScrollTop = 0;
 const header = document.getElementById('header');
 const nav = document.getElementById('nav');
 const buttonJob = document.getElementById('buttonJob');
@@ -9,6 +10,83 @@ const iconMenu = document.getElementById('iconMenu');
 const body = document.body;
 const html = document.documentElement; // Para agregar la clase también al html
 const containerResponsive = document.querySelector('.container__responsive');
+const itemContact = document.getElementById('itemContact');
+
+
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+        // Scroll hacia abajo
+        header.classList.remove('visible', 'scrolled');
+        header.classList.add('hidden');
+    } else {
+        // Scroll hacia arriba
+        header.classList.remove('hidden');
+        header.classList.add('visible');
+        if (scrollTop > 0) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    }
+    lastScrollTop = scrollTop;
+});
+
+
+
+
+
+
+
+
+
+function addClass(){
+    // Si no está activo, agregar las clases y bloquear el scroll
+    header.classList.add('container__responsive');
+    nav.classList.add('nav__responsive');
+    buttonJob.classList.add('button--responsive');
+    navList.classList.add('nav__list-responsive');
+    navItems.forEach((item) => {
+        item.classList.add('nav__item-responsive');
+    });
+    line.classList.add('line__responsive');
+    navSvgs.forEach((svg) => {
+        svg.classList.add('nav__svg-responsive');
+    });
+    iconMenu.classList.add('icon-menu--active');
+    body.classList.add('no-scroll'); // Bloquear el scroll
+    html.classList.add('no-scroll'); // Bloquear también el html
+}
+
+function removeClass(){
+    // Si está activo, quitar las clases y desbloquear el scroll
+    header.classList.remove('container__responsive');
+    nav.classList.remove('nav__responsive');
+    buttonJob.classList.remove('button--responsive');
+    navList.classList.remove('nav__list-responsive');
+    navItems.forEach((item) => {
+        item.classList.remove('nav__item-responsive');
+    });
+    line.classList.remove('line__responsive');
+    navSvgs.forEach((svg) => {
+        svg.classList.remove('nav__svg-responsive');
+    });
+    iconMenu.classList.remove('icon-menu--active');
+    body.classList.remove('no-scroll'); // Desbloquear el scroll
+    html.classList.remove('no-scroll'); // Desbloquear también el html
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Agregar el evento de clic al icono del menú
 iconMenu.addEventListener('click', () => {
@@ -16,39 +94,15 @@ iconMenu.addEventListener('click', () => {
     const isActive = iconMenu.classList.contains('icon-menu--active');
 
     if (isActive) {
-        // Si está activo, quitar las clases y desbloquear el scroll
-        header.classList.remove('container__responsive');
-        nav.classList.remove('nav__responsive');
-        buttonJob.classList.remove('button--responsive');
-        navList.classList.remove('nav__list-responsive');
-        navItems.forEach((item) => {
-            item.classList.remove('nav__item-responsive');
-        });
-        line.classList.remove('line__responsive');
-        navSvgs.forEach((svg) => {
-            svg.classList.remove('nav__svg-responsive');
-        });
-        iconMenu.classList.remove('icon-menu--active');
-        body.classList.remove('no-scroll'); // Desbloquear el scroll
-        html.classList.remove('no-scroll'); // Desbloquear también el html
-        containerResponsive.classList.remove('active');  // Desactivar animación
+        removeClass();
+        // Verificar si la opción seleccionada fue itemContact
+
     } else {
-        // Si no está activo, agregar las clases y desbloquear el scroll
-        header.classList.add('container__responsive');
-        nav.classList.add('nav__responsive');
-        buttonJob.classList.add('button--responsive');
-        navList.classList.add('nav__list-responsive');
-        navItems.forEach((item) => {
-            item.classList.add('nav__item-responsive');
-        });
-        line.classList.add('line__responsive');
-        navSvgs.forEach((svg) => {
-            svg.classList.add('nav__svg-responsive');
-        });
-        iconMenu.classList.add('icon-menu--active');
-        body.classList.add('no-scroll'); // Bloquear el scroll
-        html.classList.add('no-scroll'); // Bloquear también el html
-        containerResponsive.classList.add('active'); // Activar animación
+        addClass();
     }
+});
+
+itemContact.addEventListener('click', () => {
+    removeClass();
 });
 
