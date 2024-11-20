92a10e41c45c35cda1ede25c0b5ebd049ed5dbe2
FelipeCarvajalParra <felipiyocarvajal@gmail.com>
2024-11-20 14:46:54 -0500

    Revision 6

--- docs/JS/email.js
+++ docs/JS/email.js
@@ -2,6 +2,14 @@
   // Inicializa EmailJS con tu user_id
   emailjs.init('i0L2uutTpfLOo50XP'); // Reemplaza 'YOUR_USER_ID' con tu ID de usuario de EmailJS
 
+  // Configurar opciones de Toastr
+  toastr.options = {
+    progressBar: true,
+    "positionClass": "toast-bottom-right", // Cambia la posición según tu preferencia
+    timeOut: '5000', // 5 segundos
+    extendedTimeOut: '2000', // Tiempo extra al pasar el mouse
+  };
+
   // Selecciona el formulario y agrega un event listener para el submit
   document.querySelector('.container__form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevenir el comportamiento por defecto del formulario
@@ -16,10 +24,12 @@
     // Enviar el formulario a EmailJS
     emailjs.send('service_3fw79i3', 'template_o04wpug', formData)
       .then(function(response) {
-        alert('¡Tu mensaje ha sido enviado con éxito!');
+        // Mostrar mensaje de éxito con Toastr
+        toastr.success('Pronto me pondré en contacto contigo.', '¡Mensaje enviado correctamente!');
         document.querySelector('.container__form').reset(); // Limpiar el formulario
       }, function(error) {
-        alert('Hubo un error al enviar tu mensaje. Por favor, intenta de nuevo.');
+        // Mostrar mensaje de error con Toastr
+        toastr.error('Por favor, intenta de nuevo.', 'Hubo un error al enviar tu mensaje.');
       });
   });
 })();
