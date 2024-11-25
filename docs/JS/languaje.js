be8e753c7c22cf54867f0bac090bef649c9c5d88
FelipeCarvajalParra <felipiyocarvajal@gmail.com>
2024-11-25 17:57:04 -0500

    Lenguaje index

--- docs/JS/languaje.js
+++ docs/JS/languaje.js
@@ -1,6 +1,7 @@
 buttonLanguaje = document.getElementById('languaje-select')
 listLanguaje = document.getElementById('list-languaje')
 
+
 buttonLanguaje.addEventListener('click', (event) => {
     event.stopPropagation();
     listLanguaje.style.position = 'absolute';
@@ -18,4 +19,25 @@ document.addEventListener('click', (event) => {
         listLanguaje.style.display = 'none';
         buttonLanguaje.style.color = 'rgb(152, 152, 159)';
     }
+});
+
+const textToChange = document.querySelectorAll("[data-section]");
+
+const listLanguajes = document.querySelectorAll('.lang');
+const changeLanguaje = async language => {
+    const requestJson = await fetch(`./JS/languages/${language}.json`);
+    const texts = await requestJson.json();
+    
+    for (const element of textToChange) { // Cambié textToChange por element
+        const section = element.dataset.section;
+        const value = element.dataset.value;
+
+        element.innerHTML = texts[section][value];
+    }
+};
+
+listLanguajes.forEach((element) => {
+    element.addEventListener('click', (event) => {
+        changeLanguaje(element.dataset.language);
+    });
 });
